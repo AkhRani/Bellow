90047137a735a35048b23d4d480b43ef922d6a85
Kevin Hart <kevin.kevinhart@gmail.com>
2015-08-31 23:03:19 -0700

    Initial Lua Console
    Barely-working interface

--- LuaConsole/LuaConsole.cpp
+++ LuaConsole/LuaConsole.cpp
@@ -11,6 +11,26 @@ extern "C" {
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+  lua_State *L = luaL_newstate();
+  char buff[100];
+  printf("> ");
+  while (fgets(buff, 100, stdin)) {
+    if (buff[0] == 'q') {
+      break;
+    }
+    bool error = luaL_dostring(L, buff);
+    if (error) {
+      printf("error: %s\n> ", lua_tostring(L, -1));
+    }
+    else {
+      if (lua_isnumber(L, -1)) {
+        printf("%lf\n> ", lua_tonumber(L, -1));
+      }
+      else {
+        printf("OK\n> ");
+      }
+    }
+  }
+  return 0;
 }
 
